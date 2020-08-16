eb19030b2b5bd243663fae1cb45be6dc278faa1f
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2020-08-16 17:49:57 +0545

    Release

--- example/challenge.php
+++ example/challenge.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json");
-include "../TikTok.php";
+include "../lib/TikTok.php";
 $api = new \TikTok\Api();
 $result = $api->getChallenge("foryourpage");
 echo json_encode($result,JSON_PRETTY_PRINT);
--- example/music.php
+++ example/music.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json");
-include "../TikTok.php";
+include "../lib/TikTok.php";
 $api = new \TikTok\Api();
 $result = $api->getMusic("6798898508385585925");
 echo json_encode($result,JSON_PRETTY_PRINT);
