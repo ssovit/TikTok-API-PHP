0dbc22efda04a3f92f40a0e599f8e7150a46f8a1
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2020-09-28 11:36:56 +0545

    g

--- example/download-video.php
+++ example/download-video.php
@@ -1,5 +1,4 @@
 <?php
-header("Content-Type: application/json");
 include __DIR__."/../vendor/autoload.php";
 $api = new \Sovit\TikTok\Api();
 $result = $api->getVideoByUrl("https://www.tiktok.com/@zachking/video/6829303572832750853");
